4f8cc13066cf1ea68bd0c995daa0d72c80531854
Lande <laande.pro@gmail.com>
2026-01-28 20:31:32 +0100

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.animatedwidgets"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "2"
+        versionCode = 7
+        versionName = "2.0"
     }
 
     buildTypes {
